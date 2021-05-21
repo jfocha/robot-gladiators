c6cb006125c4c41e9ec5a3a880967ffb0e382989
jfocha <jfocha@gmail.com>
2021-05-20 17:34:08 -0700

    added multiple battles

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,10 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+      // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  //window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -61,8 +63,11 @@ if (playerHealth <= 0) {
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
+}
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
   }
